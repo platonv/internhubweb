365cb714f0af17746ee86ffc19e89dcf6b476212
Catalin <andreimesesan@yahoo.com>
2017-04-27 23:47:27 +0300

    "JobsList Collapsable"

--- internHub/src/app/pages/jobs/jobs.component.ts
+++ internHub/src/app/pages/jobs/jobs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Angular2TokenService } from '../../services/token-service/auth-token.service';
 import { JobsService, Job} from './jobs.service';
+import'jquery';
 
 @Component({
   providers:[JobsService],
